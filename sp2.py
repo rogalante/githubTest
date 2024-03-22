c41b35f58d0dfa1a2327ea8eaef8adcda9e819ac
rogalante <159934145+rogalante@users.noreply.github.com>
2024-03-22 11:00:39 -0400

    Create sp2.py

---
+++ sp2.py
@@ -0,0 +1,31 @@
+import unidecode
+import sys
+
+iFile = sys.argv[1]
+oFile = sys.argv[2]
+converAll = sys.argv[3]
+print (iFile)
+print (oFile)
+print (converAll)
+#input file
+fin = open(iFile, "rt")
+#output file to write the result to
+fout = open(oFile, "wt")
+#for each line in the input file
+x=0
+
+for line in fin:
+	#read replace the string and write to output file
+	#fout.write(line.replace('pyton', 'python'))
+	#print(line)
+	if converAll == "All":
+		fout.write( unidecode.unidecode(line))
+	else:
+		x+=1
+		if x ==1:
+			fout.write( unidecode.unidecode(line))
+		else:
+			fout.write( line)
+#close input and output files
+fin.close()
+fout.close()
